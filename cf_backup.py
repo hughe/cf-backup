b5c2c93ee89b94e5ab1ba9df9db7f2a0389a9614
Hugh Emberson <hugh.emberson@gmail.com>
2024-06-25 20:20:27 -0600

    Add skeleton for UI.

--- cf_backup.py
+++ cf_backup.py
@@ -1,10 +1,44 @@
-# use thw clock asp as a placeholder for now.
-from time import asctime, sleep, gmtime
+from pt_miniscreen.core import App
+from pt_miniscreen.core import Component
 
-from pitop.miniscreen import Miniscreen
+class BackupUI(Component):
+  # define a `default_state` dictionary to create state with known values
+  default_state = {
+  }
 
-m = Miniscreen()
-while True:
-    t = gmtime()
-    m.display_text(asctime(t), font_size=8)
-    sleep(1)
+  def __init__(self, **kwargs):
+    super().__init__(**kwargs)
+
+  def select_button_pressed(self):
+      pass
+  
+  def render(self, image):
+      pass
+    
+class BackupApp(App):
+  def __init__(self, miniscreen):
+    super().__init__(display=miniscreen.device.display, Root=BackupUI)
+
+    # We call methods on self, because self.root does not exist until
+    # start() is run. So if we said `self.root.button_pressed` we'd
+    # get an exception.
+    miniscreen.select_button.when_pressed = self.select_button_pressed
+    miniscreen.cancel_button.when_pressed = self.cancel_button_pressed
+
+  def select_button_pressed(self):
+    self.root.select_button_pressed()
+
+  def cancel_button_pressed(self):
+    self.stop()
+
+def main():
+  m = Miniscreen()
+
+  a = BackupApp(m)
+  
+  a.start()
+  a.wait_for_stop()
+
+if __name__ == '__main__':
+  main()
+
